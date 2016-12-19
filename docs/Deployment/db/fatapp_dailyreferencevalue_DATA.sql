b310b426e94dc5f8c51c1e30f13b0faa057bfd0c
fkucuk <fkucuk@gmail.com>
2016-12-19 14:24:11 +0300

    unnecessary files deleted

---
+++ docs/Deployment/db/fatapp_dailyreferencevalue_DATA.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: fatapp
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `dailyreferencevalue`
+--
+
+LOCK TABLES `dailyreferencevalue` WRITE;
+/*!40000 ALTER TABLE `dailyreferencevalue` DISABLE KEYS */;
+INSERT INTO `dailyreferencevalue` VALUES (1,'Total Fat',65,'g',NULL),(2,'Saturated Fat',20,'g',NULL),(3,'Cholesterol',300,'mg',NULL),(4,'Sodium',2400,'mg',NULL),(5,'Potassium',3500,'mg',NULL),(6,'Total Carbohydrate',300,'g',NULL),(7,'Dietary Fiber',25,'g',NULL),(8,'Protein',50,'g',NULL),(9,'Vitamin A',5000,'IU',NULL),(10,'Vitamin C',60,'mg',NULL),(11,'Calcium',1000,'mg',NULL),(12,'Iron',18,'mg',NULL),(13,'Vitamin D',400,'IU',NULL),(14,'Vitamin E',30,'IU',NULL),(15,'Vitamin K',80,'µg',NULL),(16,'Thiamin',1.5,'mg',NULL),(17,'Riboflavin',1.7,'mg',NULL),(18,'Niacin',20,'mg',NULL),(19,'Vitamin B6',2,'mg',NULL),(20,'Folate',400,'µg',NULL),(21,'Vitamin B12',6,'µg',NULL),(22,'Biotin',300,'µg',NULL),(23,'Pantothenic acid',10,'mg',NULL),(24,'Phosphorus',1000,'mg',NULL),(25,'Iodine',150,'µg',NULL),(26,'Magnesium',400,'mg',NULL),(27,'Zinc',15,'mg',NULL),(28,'Selenium',70,'µg',NULL),(29,'Copper',2,'mg',NULL),(30,'Manganese',2,'mg',NULL),(31,'Chromium',120,'µg',NULL),(32,'Molybdenum',75,'µg',NULL),(33,'Chloride',3400,'mg',NULL),(34,'Total Calories',2000,'calories',NULL);
+/*!40000 ALTER TABLE `dailyreferencevalue` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-19  6:28:07
